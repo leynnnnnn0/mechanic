10d4348f3b14a77ea07d83c7cb6473c0d8749674
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-09-12 21:59:03 +0800

    added delete function

--- app/Filament/Resources/CarResource.php
+++ app/Filament/Resources/CarResource.php
@@ -57,7 +57,9 @@ class CarResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
