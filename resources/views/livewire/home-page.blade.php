f83ec20468e831ede21b7c9228a3fea515c46014
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-09-18 17:23:34 +0800

    finished temporary landing page

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -1,14 +1,14 @@
 <div class="relative">
      <!-- Nav bar -->
-     <section class="fixed top-0 z-50 px-[200px] w-full">
+     <section class="fixed top-0 z-50 w-full">
           <livewire:navigation />
      </section>
      <!-- Home Page -->
-     <section class="px-[200px] relative flex items-center justify-center min-h-screen">
+     <section class="lg:px-[400px] md:px-[200px] relative flex items-center justify-center min-h-screen">
           <section class="space-y-5 mb-16">
-               <h1 class="text-6xl font-bold ">AUTO MAINTENANCE,<br /> SERVICE & REPAIR</h1>
+               <h1 class="text-6xl font-bold text-blue-500">AUTO MAINTENANCE,<br /> SERVICE & REPAIR</h1>
                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore sit libero quo rerum quasi atque consectetur dolorum saepe reiciendis, modi deleniti voluptate aspernatur eveniet? Nesciunt assumenda laborum delectus laboriosam odio?</p>
-               <button class="border border-black px-4 py-1 text-lg font-bold rounded-sm">
+               <button class="px-4 py-1 text-lg bg-blue-500 font-bold rounded-sm text-white">
                     Book an appointment
                </button>
           </section>
@@ -18,8 +18,8 @@
      </section>
      <!-- Why Choose Us? -->
      <!-- Easy Booking process Real time updates warranty flat rate pricing customer support -->
-     <section class="h-[600px] flex flex-col justify-center items-center p-5 gap-10">
-          <h1 class="text-3xl font-bold">
+     <section class="h-[400px] bg-blue-50 flex flex-col justify-center items-center p-5 gap-10">
+          <h1 class="text-3xl font-bold text-blue-500">
                Why Choose Us?
           </h1>
           <div class="flex items-center gap-5 justify-center">
@@ -45,6 +45,23 @@
                     <p class="text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, iste. Lorem ipsum dolor sit amet.</p>
                </div>
           </div>
+     </section>
+     <!-- our team -->
+     <section class="h-[600px] flex flex-col justify-center items-center p-5 gap-10">
+          <h1 class="text-3xl font-bold text-blue-500">
+               Our Trusted Mechanics
+          </h1>
+          <div class="flex items-center justify-center gap-5">
+               <img src="{{ Vite::asset('resources/images/mec1.png')}}" alt="">
+               <img src="{{ Vite::asset('resources/images/mec2.png')}}" alt="">
+               <img src="{{ Vite::asset('resources/images/mec3.png')}}" alt="">
+               <img src="{{ Vite::asset('resources/images/mec4.png')}}" alt="">
+               <img src="{{ Vite::asset('resources/images/mec5.png')}}" alt="">
+          </div>
 
      </section>
+
+     <footer class="px-[200px] h-16 flex items-center border-t border-gray-300">
+          <h1>© 2024 mecahanic.com. All rights reserved.</h1>
+     </footer>
 </div>
