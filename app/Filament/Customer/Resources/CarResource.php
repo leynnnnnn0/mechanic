5e6ba072c63cba76a44cc270e61a0d498ebfceb0
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-09-20 16:15:50 +0800

    added car resource for customer page

---
+++ app/Filament/Customer/Resources/CarResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Customer\Resources;
+
+use App\Filament\Customer\Resources\CarResource\Pages;
+use App\Filament\Customer\Resources\CarResource\RelationManagers;
+use App\Models\Car;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CarResource extends Resource
+{
+    protected static ?string $model = Car::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-truck';
+    protected static ?string $navigationLabel = 'My Vehicles';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('make'),
+                TextColumn::make(name: 'model'),
+                TextColumn::make('year'),
+                TextColumn::make('color'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCars::route('/'),
+            'create' => Pages\CreateCar::route('/create'),
+            'edit' => Pages\EditCar::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->where('customer_id', auth()->user()->customer_id);
+    }
+}
---
+++ app/Filament/Customer/Resources/CarResource/Pages/ListCars.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Customer\Resources\CarResource\Pages;
+
+use App\Filament\Customer\Resources\CarResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCars extends ListRecords
+{
+    protected static string $resource = CarResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
