0b0a526ce6bf6a3dba2436fed82aa193c98f68db
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-09-21 08:06:05 +0800

    added tab section in appointment

--- app/Filament/Resources/AppointmentResource/Pages/ListAppointments.php
+++ app/Filament/Resources/AppointmentResource/Pages/ListAppointments.php
@@ -2,25 +2,42 @@
 
 namespace App\Filament\Resources\AppointmentResource\Pages;
 
+use App\Enum\AppointmentStatus;
 use App\Filament\Resources\AppointmentResource;
+use App\Models\Appointment;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+
 
 class ListAppointments extends ListRecords
 {
+
+    public function getTabs(): array
+    {
+        $tabs = [];
+
+        $tabs[] = Tab::make('All Appointments')
+            ->badge(Appointment::count());
+
+        foreach (AppointmentStatus::cases() as $status) {
+            $status = $status->value;
+            $tabs[] = Tab::make(ucfirst($status))
+                ->badge(Appointment::where('status', $status)->count())
+                ->modifyQueryUsing(function ($query) use ($status) {
+                    return $query->where('status', $status);
+                });
+        }
+
+
+        return $tabs;
+    }
     protected static string $resource = AppointmentResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\CreateAction::make(),
-        ];
-    }
-
-    protected function getHeaderWidgets(): array
-    {
-        return [
-            AppointmentResource\Widgets\AppointmentOverview::class,
         ];
     }
 }
