6ef1b5e4e3500e552d65ca7a6f2680be25924da1
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-09-22 12:59:38 +0800

    prep for fly launch

--- app/Filament/Mechanic/Resources/AppointmentResource/Pages/CreateAppointment.php
+++ app/Filament/Mechanic/Resources/AppointmentResource/Pages/CreateAppointment.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Mechanic\Resources\AppointmentResource\Pages;
 
 use App\Filament\Mechanic\Resources\AppointmentResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateAppointment extends CreateRecord
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,3 @@
 import "./bootstrap";
 import "../css/app.css";
+import.meta.glob(["../images/**"]);
